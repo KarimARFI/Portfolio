f7602ac978d0b1903856c09a2e1267c02603cf8c
Karim <100566437+KarimARFI@users.noreply.github.com>
2023-03-05 21:23:46 +0100

    ajout de la partie réalistion

---
+++ App/home.js
@@ -0,0 +1,7 @@
+'use strict';
+let home = document.getElementsByTagName('body');
+let div = document.createElement('div');
+// home.prepend(div);
+home.innerHTML=div;
+console.log(home);
+console.log(div);
